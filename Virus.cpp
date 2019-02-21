efd0cb3fd9ae2fba48e1185b20bd07d1236cd8d2
jaisho <44206270+jaisho@users.noreply.github.com>
2019-02-21 20:52:44 +0100

    Update Virus.cpp

--- Virus.cpp
+++ Virus.cpp
@@ -1,5 +1,5 @@
 ﻿/********************************************		            
-* If you're here, you should know that      *			      
+* If you're here, you should know that      *		Jan	      
 * I'm the author of this code. I wrote it   *			      
 * because I wanted to punish my roommate    *			  
 * for touching my computer when I was away  *			 
